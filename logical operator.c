2db938e0a52cb1f7d89f350d78332dd8a978825b
Mani rathna kumar.V <rathnakumar30062006@gmail.com>
2026-02-16 19:48:54 +0530

    Add files via upload

---
+++ logical operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+void main() {
+    int a=1,b=0,c=2,d=0;
+    float AND,OR,NOT;
+    AND=a&&b;
+    OR=c||d;
+    NOT=!a;
+    printf("%f\n%f\n%f",AND,OR,NOT);
+    
+    
+    getch();
+}
